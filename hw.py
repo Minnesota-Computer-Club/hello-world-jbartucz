226361925ba17731f73e35193a585655c1327b84
John Bartucz <john@bartucz.com>
2024-05-06 19:04:23 +0000

    first try

--- hw.py
+++ hw.py
@@ -1,3 +1,5 @@
 # Hello, traveler! Your first task is to say hello to the entire world you have entered.
 # Edit this file so that it prints out "Hello, world!" (without the quotation marks) 
 # Make sure to use the Python programming language!
+
+print("asdf")
