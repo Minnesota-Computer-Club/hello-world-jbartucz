f5645887fdee1d3fdd2be8c687fae19f17e97759
John Bartucz <114032131+drbartucz@users.noreply.github.com>
2024-05-06 13:54:27 -0500

    Create hw.py

---
+++ hw.py
@@ -0,0 +1,3 @@
+# Hello, traveler! Your first task is to say hello to the entire world you have entered.
+# Edit this file so that it prints out "Hello, world!" (without the quotation marks) 
+# Make sure to use the Python programming language!
